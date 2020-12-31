6a42956ef4760d02eae420c81f0757dcbfe6087d
fujihiraryo <fujihiraryo.team-lab.com>
2020-12-31 19:07:03 +0900

    ナップサック問題(半分全列挙)

--- DPL/01H_HugeKnapsackProblem.py
+++ DPL/01H_HugeKnapsackProblem.py
@@ -1,41 +1,41 @@
 import bisect
 
-INF = 10 ** 20
 n, m = map(int, input().split())
 v, w = [], []
 for _ in range(n):
     vi, wi = map(int, input().split())
     v.append(vi)
     w.append(wi)
+# 前半の全列挙
 n0 = n // 2
-wv0 = []
-for s in range(1 << n0):
-    vi, wi = 0, 0
-    for i in range(n0):
-        if (1 << i) & s:
-            vi += v[i]
-            wi += w[i]
-    wv0.append([wi, vi])
-wv0.sort()
+v0, w0 = [0], [0]
+cnt = 1
+for i in range(n0):
+    for j in range(cnt):
+        v0.append(v[i] + v0[j])
+        w0.append(w[i] + w0[j])
+        cnt += 1
+idx = sorted(range(1 << n0), key=lambda i: w0[i])
+v0 = [v0[i] for i in idx]
+w0 = [w0[i] for i in idx]
 tmp = 0
 for i in range(1 << n0):
-    if tmp > wv0[i][1]:
-        wv0[i][1] = tmp
-    tmp = max(tmp, wv0[i][1])
+    if tmp > v0[i]:
+        v0[i] = tmp
+    tmp = max(tmp, v0[i])
+# 後半の全列挙
 n1 = n - n0
-wv1 = []
-for s in range(1 << n1):
-    vi, wi = 0, 0
-    for i in range(n1):
-        if (1 << i) & s:
-            vi += v[i + n0]
-            wi += w[i + n0]
-    wv1.append([wi, vi])
+v1, w1 = [0], [0]
+cnt = 1
+for i in range(n1):
+    for j in range(cnt):
+        v1.append(v[i + n0] + v1[j])
+        w1.append(w[i + n0] + w1[j])
+        cnt += 1
+# 二分探索
 ans = 0
-for x in wv1:
-    if x[0] <= m:
-        i = max(0, bisect.bisect_right(wv0, [m - x[0], INF]) - 1)
-        ans = max(ans, x[1] + wv0[i][1])
+for i in range(1 << n1):
+    if w1[i] <= m:
+        j = max(0, bisect.bisect_right(w0, m - w1[i]) - 1)
+        ans = max(ans, v0[j] + v1[i])
 print(ans)
-print(wv0)
-print(wv1)
