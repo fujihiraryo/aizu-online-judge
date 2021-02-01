2a1c56e81851fc5767b17f6233f9cb66a2e17035
fujihiraryo <fujihiraryo.team-lab.com>
2021-02-01 13:02:25 +0900

    平方分割

--- DSL/02C_RangeSearch.py
+++ DSL/02C_RangeSearch.py
@@ -1,8 +1,3 @@
-import sys
-
-input = sys.stdin.readline
-
-
 def lower_bound(a, x, key=lambda i: i):
     n = len(a)
     i, j = -1, n
@@ -27,55 +22,49 @@ def upper_bound(a, x, key=lambda i: i):
     return j
 
 
-def merge(a, b, key=lambda i: i):
-    i, j = 0, 0
-    c = []
-    while i < len(a) and j < len(b):
-        if key(a[i]) <= key(b[j]):
-            c.append(a[i])
-            i += 1
-        else:
-            c.append(b[j])
-            j += 1
-    return c + a[i:] + b[j:]
-
-
 class RangeSearch:
     def __init__(self, x, y):
+        key_x = lambda i: x[i]
+        key_y = lambda i: y[i]
+        n = len(x)
+        d = 1
+        while d ** 2 < n:
+            d += 1
+        array = list(range(n))
+        array.sort(key=key_x)
+        bucket = [[] for _ in range(d)]
+        for i in range(n):
+            bucket[i // d].append(array[i])
+        for i in range(d):
+            bucket[i].sort(key=key_y)
+        self.size = d
         self.x = x
         self.y = y
-        n = len(x)
-        self.size = 1 << n.bit_length()
-        self.tree = [[] for _ in range(self.size << 1)]
-        idx = list(range(n))
-        idx.sort(key=lambda i: x[i])
-        self.idx = idx
-        for i in range(n):
-            self.tree[i + self.size] = [idx[i]]
-        for i in range(1, self.size)[::-1]:
-            left = self.tree[2 * i]
-            right = self.tree[2 * i + 1]
-            self.tree[i] = merge(left, right, key=lambda i: y[i])
+        self.key_x = key_x
+        self.key_y = key_y
+        self.array = array
+        self.bucket = bucket
 
     def query(self, sx, tx, sy, ty):
-        ix = lower_bound(self.idx, sx, key=lambda i: self.x[i])
-        jx = upper_bound(self.idx, tx, key=lambda i: self.x[i])
-        ix += self.size
-        jx += self.size
-        while ix < jx:
-            if ix & 1:
-                iy = lower_bound(self.tree[ix], sy, key=lambda i: self.y[i])
-                jy = upper_bound(self.tree[ix], ty, key=lambda i: self.y[i])
-                for k in range(iy, jy):
-                    yield self.tree[ix][k]
-                ix += 1
-            if jx & 1:
-                iy = lower_bound(self.tree[jx - 1], sy, key=lambda i: self.y[i])
-                jy = upper_bound(self.tree[jx - 1], ty, key=lambda i: self.y[i])
-                for k in range(iy, jy):
-                    yield self.tree[jx - 1][k]
-            ix >>= 1
-            jx >>= 1
+        d = self.size
+        ix = lower_bound(self.array, sx, key=self.key_x)
+        jx = upper_bound(self.array, tx, key=self.key_x)
+        if ix // d == jx // d:
+            for k in self.array[ix:jx]:
+                if sy <= self.y[k] <= ty:
+                    yield k
+            return
+        for k in self.array[ix : (ix // d + 1) * d]:
+            if sy <= self.y[k] <= ty:
+                yield k
+        for bucket in self.bucket[ix // d + 1 : jx // d]:
+            iy = lower_bound(bucket, sy, key=self.key_y)
+            jy = upper_bound(bucket, ty, key=self.key_y)
+            for k in bucket[iy:jy]:
+                yield k
+        for k in self.array[(jx // d) * d : jx]:
+            if sy <= self.y[k] <= ty:
+                yield k
 
 
 def main():
