fa7e1aa726e86ec30da86c4292beddcfd94c77fe
fujihiraryo <fujihiraryo.team-lab.com>
2021-02-06 20:24:47 +0900

    抽象化平方分割バケット

--- DSL/02A_RangeMinimumQuery.py
+++ DSL/02A_RangeMinimumQuery.py
@@ -1,39 +1,55 @@
-class RMQ:
-    def __init__(self, n, INF=2 ** 31 - 1):
+class Bucket:
+    def __init__(self, a, ide, fold, apply):
+        n = len(a)
         d = 1
-        while d ** 2 <= n:
+        while d ** 2 < n:
             d += 1
         self.size = d
-        self.bucket = [[INF] * d for _ in range(d)]
-        self.bucket_min = [INF] * d
-        self.min = lambda x: min(x, default=INF)
+        self.ide = ide
+        self.fold = fold
+        self.apply = apply
+        self.data = a
+        self.block = [ide] * d
+        for i in range(d):
+            self.block_update(i)
 
-    def update(self, k, x):
+    def block_update(self, i):
         d = self.size
-        i, j = k // d, k % d
-        self.bucket[i][j] = x
-        self.bucket_min[i] = min(self.bucket[i])
+        x = self.ide
+        for y in self.data[i * d : i * d + d]:
+            x = self.fold(x, y)
+        self.block[i] = x
 
-    def range(self, k0, k1):
+    def point_apply(self, i, f):
         d = self.size
-        i0, j0 = k0 // d, k0 % d
-        i1, j1 = k1 // d, k1 % d
-        if i0 == i1:
-            return min(self.bucket[i0][j0:j1])
-        return min(
-            self.min(self.bucket_min[i0 + 1 : i1]),
-            self.min(self.bucket[i0][j0:d]),
-            self.min(self.bucket[i1][0:j1]),
-        )
+        self.data[i] = self.apply(f, self.data[i])
+        self.block_update(i // d)
+
+    def range_fold(self, i, j):
+        d = self.size
+        x = self.ide
+        if i // d == j // d:
+            for y in self.data[i:j]:
+                x = self.fold(x, y)
+            return x
+        for y in self.data[i : -(-i // d) * d]:
+            x = self.fold(x, y)
+        for y in self.block[-(-i // d) : j // d]:
+            x = self.fold(x, y)
+        for y in self.data[(j // d) * d : j]:
+            x = self.fold(x, y)
+        return x
 
 
 n, q = map(int, input().split())
-srd = RMQ(n)
+ide = (1 << 31) - 1
+a = [ide] * n
+bucket = Bucket(a, ide, min, lambda y, x: y)
 for _ in range(q):
     (*query,) = map(int, input().split())
     if query[0] == 0:
-        i, x = query[1], query[2]
-        srd.update(i, x)
+        i, x = query[1:]
+        bucket.point_apply(i, x)
     else:
-        i, j = query[1], query[2]
-        print(srd.range(i, j + 1))
+        i, j = query[1:]
+        print(bucket.range_fold(i, j + 1))
